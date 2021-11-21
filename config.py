4f51ee1b2170d575e929a9312ab451beadfc3432
ManHinnn0509 <cheungmanhin0509@gmail.com>
2021-11-22 05:45:54 +0800

    Add current progress

---
+++ config.py
@@ -0,0 +1,6 @@
+TITLE = 'MP3 Player'
+WINDOW_SIZE = "600x300"
+
+# Allow resize or not
+RESIZE_H = False
+RESIZE_W = False
---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+
+import tkinter as tk
+from tkinter import *
+
+from config import *
+
+class MP3Player:
+    def __init__(self, master: tk.Tk) -> None:
+        self.master = master
+
+        master.title(TITLE)
+        master.geometry(WINDOW_SIZE)
+        master.resizable(RESIZE_W, RESIZE_H)
+
+        frame = Frame(master)
+        self.frame = frame
+
+        listbox = Listbox(frame)
+        self.listbox = listbox
+
+    def __addContent(self):
+        # https://stackoverflow.com/questions/46625722/how-to-list-files-in-a-folder-to-a-tk-listbox-python3
+        dirContent = [i for i in os.listdir() if (i.endswith('.mp3'))]
+
+        counter = 1
+        for mp3 in dirContent:
+            self.listbox.add
+
+def main():
+    root = tk.Tk()
+    mp3Player = MP3Player(root)
+
+    root.mainloop()
+
+if (__name__ == '__main__'):
+    main()
